d1a472fa5a9451e1df052a3ed67bbb0e47913c49
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-03-31 23:51:41 -0400

    feat: updated routers index based on updated skeleton design

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,11 @@
-from . import orders, order_details
+from . import orders, order_details, customers, payments, promotions, ratings, sandwiches
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(customers.router)
+    app.include_router(payments.router)
+    app.include_router(promotions.router)
+    app.include_router(ratings.router)
+    app.include_router(sandwiches.router)
