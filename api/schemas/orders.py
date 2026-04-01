4c112e9fcd79f7a10cde33aeed6eff2c7595c785
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-03-31 23:24:12 -0400

    feat: added orders schema

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -1,28 +1,40 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
 from .order_details import OrderDetail
+from .customers import Customer
+from .promotions import Promotion
 
 
 
-class OrderBase(BaseModel):
+class OrderCreate(BaseModel):
     customer_name: str
     description: Optional[str] = None
-
-
-class OrderCreate(OrderBase):
-    pass
+    tracking_number: str
+    delivery_type: str
+    total_price: float
+    customer_id: int
+    promotion_id: Optional[int] = None
 
 
 class OrderUpdate(BaseModel):
     customer_name: Optional[str] = None
     description: Optional[str] = None
+    tracking_number: Optional[str] = None
+    order_status: Optional[str] = None
+    delivery_type: Optional[str] = None
+    total_price: Optional[float] = None
+    customer_id: Optional[int] = None
+    promotion_id: Optional[int] = None
 
 
-class Order(OrderBase):
+class Order(OrderCreate):
     id: int
-    order_date: Optional[datetime] = None
+    order_date: datetime
+    order_status: str
     order_details: list[OrderDetail] = None
+    customer: Optional[Customer] = None
+    promotion: Optional[Promotion] = None
 
     class ConfigDict:
         from_attributes = True
