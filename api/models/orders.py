cd5fb4fb03c8c775bc03de045f44ec6d204163cc
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-03-31 23:09:48 -0400

    feat: added orders model

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, func
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
@@ -9,7 +9,16 @@ class Order(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_name = Column(String(100))
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    order_date = Column(DATETIME, nullable=False, server_default=str(func.now()))
     description = Column(String(300))
+    tracking_number = Column(String(50), unique=True, nullable=False, index=True)
+    order_status = Column(String(20), default="Pending")
+    delivery_type = Column(String(20), nullable=False)
+    total_price = Column(DECIMAL(10, 2), nullable=False)
+    customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
+    promotion_id = Column(Integer, ForeignKey("promotions.id"))
 
+    customer = relationship("Customer", back_populates="orders")
     order_details = relationship("OrderDetail", back_populates="order")
+    payment = relationship("Payment", back_populates="order", uselist=False)
+    promotion = relationship("Promotion", back_populates="order")
