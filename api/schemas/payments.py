b9e56ce81b1d52baa92924072f9e70d433e877f0
Johnn_Chen <jchen84@charlotte.edu>
2026-04-05 20:58:54 -0400

    Code for schemas: payment

---
+++ api/schemas/payments.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+class PaymentBase(BaseModel):
+    order_id: int
+    card_number: str
+    card_holder: str
+    expiration_date: str
+    payment_type: str
+
+
+class PaymentCreate(PaymentBase):
+    pass
+
+
+class PaymentUpdate(BaseModel):
+    card_number: Optional[str] = None
+    card_holder: Optional[str] = None
+    expiration_date: Optional[str] = None
+    transaction_status: Optional[str] = None
+    payment_type: Optional[str] = None
+
+
+class Payment(PaymentBase):
+    id: int
+    transaction_status: str
+    transaction_date: Optional[datetime] = None
+
+    class ConfigDict:
+        from_attributes = True
